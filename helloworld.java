ba997d432a71ce0370c76208b18c31206d34e0a1
Stefan Ziegler <edi.gonzales@gmail.com>
2020-11-18 15:55:38 +0100

    output jdk version

--- helloworld.java
+++ helloworld.java
@@ -5,7 +5,20 @@ import static java.lang.System.*;
 
 public class helloworld {
 
+ //   public static List<String> OEREB_SERVICE_BASE_URL = Stream.of(
+//            "https://oereb.geo.sh.ch/oereb/", 
+//            "https://maps.zh.ch/oereb/",
+//            "https://map.geo.gl.ch/oereb/wsgi/oereb/",
+//            "https://map.geo.sz.ch/oereb/wsgi/oereb/",
+//            "https://api.geo.ag.ch/v1/oereb/",
+//            "https://geo.so.ch/api/oereb/")
+//            .collect(Collectors.toList());
+
+
+
     public static void main(String... args) {
         out.println("Hello Stefan");
+
+        out.println(System.getProperty("java.version") );
     }
 }
